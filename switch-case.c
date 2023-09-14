099901e29a4866b5b304807dba7e58ca495c99a3
Mohammed Elarbi El Hattab <elhattabmohammedelarbi@gmail.com>
2023-09-14 06:59:52 -0700

    voyelle et non voyelle

---
+++ switch-case.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char caractere;
+    printf("Entrez un caractere : ");
+    scanf(" %c", &caractere);
+    caractere = tolower(caractere);
+switch (caractere) {
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+printf("%c est une voyelle.\n", caractere);
+break;
+default:
+printf("%c n'est pas une voyelle.\n", caractere);
+}
+return 0;
+}
